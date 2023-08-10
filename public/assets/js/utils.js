4dca963d4a1ad202711d3a51ab2b873cf0ae4a85
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-08-10 21:41:36 +1000

    changed read function

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const util = require("util");
 
 // Promise version of fs.readFile
-const readFromFile = util.promisify(fs.readFile);
+const readFromFile = fs.readFileSync;
 /**
  *  Function to write data to the JSON file given a destination and some content
  *  @param {string} destination The file you want to write to.
